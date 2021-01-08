7f21c2964726947afc8449cfff7fb78588293222
Marianne <Marianne@DESKTOP-R0MM6V2.mshome.net>
2021-01-08 16:47:01 +0100

    ça marche !

--- ProjetApplication/src/fr/eni/projetEncheres/dal/UtilisateursDal.java
+++ ProjetApplication/src/fr/eni/projetEncheres/dal/UtilisateursDal.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
@@ -18,7 +19,7 @@ public class UtilisateursDal {
 	private static final String INSERT = "INSERT INTO UTILISATEURS(pseudo, nom, prenom, email, telephone, rue, code_postal, ville, mot_de_passe, credit, administrateur ) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
     private static final String GET_BY_NO_UTILISATEUR = "SELECT * FROM UTILISATEURS WHERE no_utilisateur = ?";
     private static final String GET_ALL = "SELECT * FROM UTILISATEURS";
-    private static final String UPDATE = "UPDATE UTILISATEURS SET pseudo = ?, nom = ?, prenom = ?, email = ?, rue = ?, code_postal = ?, ville = ?, mot_de_passe = ?, credit = ?, administrateur= ? WHERE no_utilisateur = ?";
+    private static final String UPDATE = "UPDATE UTILISATEURS SET pseudo = ?, nom = ?, prenom = ?, email = ?, telephone=?, rue = ?, code_postal = ?, ville = ?, mot_de_passe = ?, credit = ?, administrateur= ? WHERE no_utilisateur = ?";
     private static final String DELETE = "DELETE UTILISATEURS WHERE no_utilisateur = ?";
 
     private static Logger logger = MonLogger.getLogger("ProjetApplication");
@@ -30,7 +31,7 @@ public class UtilisateursDal {
     {
     	try(Connection cnx = Utils.getConnection())
     	{
-    		PreparedStatement rqt = cnx.prepareStatement(INSERT);
+    		PreparedStatement rqt = cnx.prepareStatement(INSERT, Statement.RETURN_GENERATED_KEYS);
     		rqt.setString(1, utilisateur.getPseudo());
     		rqt.setString(2, utilisateur.getNom());
     		rqt.setString(3, utilisateur.getPrenom());
@@ -42,11 +43,18 @@ public class UtilisateursDal {
     		rqt.setString(9, utilisateur.getMot_de_passe());
     		rqt.setInt(10, utilisateur.getCredit());
     		rqt.setInt(11, utilisateur.getAdministrateur());
-    		rqt.executeUpdate();
     		
     		
+    		int nbRows = rqt.executeUpdate();
+			if (nbRows == 1) {
+				ResultSet rs = rqt.getGeneratedKeys();
+				if (rs.next()) {
+					utilisateur.setNo_utilisateur(rs.getInt(1));
+				}
+			}
 		} catch (SQLException ex) {
 			logger.severe("Erreur dans la methode insert(Utilisateurs utilisateur)avec utilisateur = " + utilisateur + "-erreur :" + ex.getMessage()); 
+			ex.printStackTrace();
 		}	
     	
     }
@@ -68,6 +76,7 @@ public class UtilisateursDal {
     			result.setNom(rs.getString("nom"));
     			result.setPrenom(rs.getString("prenom"));
     			result.setEmail(rs.getString("email"));
+    			result.setTelephone(rs.getString("telephone"));
     			result.setRue(rs.getString("rue"));
     			result.setCode_postal(rs.getString("code_postal"));
     			result.setVille(rs.getString("ville"));
@@ -121,11 +130,12 @@ public class UtilisateursDal {
     		rqt.setString(9, utilisateur.getMot_de_passe());
     		rqt.setInt(10, utilisateur.getCredit());
     		rqt.setInt(11, utilisateur.getAdministrateur());
-    		
+    		rqt.setInt(12, utilisateur.getNo_utilisateur());
     		
     		rqt.executeUpdate();
+    		
     	} catch (SQLException e) {
-			e.printStackTrace();
+    		logger.severe("Erreur dans la méthode update(Utilisateurs utilisateur) avec utilisateur =" + utilisateur.toString() + " Erreur : " + e.getMessage());
 		}
     }
     
